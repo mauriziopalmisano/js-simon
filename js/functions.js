ce5ff5d32471b7e76003c3c606677b48201be8df
mauriziopalmisano <maurizio.palmisano1@gmail.com>
2026-04-07 20:48:57 +0200

    lettura risposte e confronto

--- js/functions.js
+++ js/functions.js
@@ -1,32 +1,48 @@
 "use strict";
 
 function random_number_generator(min, max, array_length) {
-  let number_array = [];
-  while (number_array.length < array_length) {
-    let random_number = Math.floor(Math.random() * max - min + 1 + min);
-    if (!number_array.includes(random_number)) {
-      number_array.push(random_number);
+    let number_array = [];
+    while (number_array.length < array_length) {
+        let random_number = Math.floor(Math.random() * max - min + 1 + min);
+        if (!number_array.includes(random_number)) {
+            number_array.push(random_number);
+        }
     }
-  }
-  return number_array;
+    return number_array;
 }
 
 function creaCountdown(timer, countdown_id) {
-  let tempo = timer;
-  let display = countdown_id;
-  let intervallo = setInterval(function () {
-    display.innerText = tempo;
-    tempo = tempo - 1;
-    if (tempo < 0) {
-      clearInterval(intervallo);
-      d_none_toggle();
-    }
-  }, 1000);
+    let tempo = timer;
+    let display = countdown_id;
+    let intervallo = setInterval(function () {
+        display.innerText = tempo;
+        tempo = tempo - 1;
+        if (tempo < 0) {
+            clearInterval(intervallo);
+            d_none_toggle();
+        }
+    }, 1000);
 }
 
 function d_none_toggle() {
-  countdown.classList.toggle("d-none");
-  instructures.classList.toggle("d-none");
-  answers_form.classList.toggle("d-none");
-  numbers_list.classList.toggle('d-none');
+    countdown.classList.toggle("d-none");
+    instructures.classList.toggle("d-none");
+    answers_form.classList.toggle("d-none");
+    numbers_list.classList.toggle("d-none");
+}
+
+function answers_check(array_number, list_answers) {
+    const array_answers = [];
+    for (let i = 0; i < list_answers.length; i++) {
+        array_answers.push(parseInt(list_answers[i].value))
+    }
+    let array_result = [];
+    console.log(array_number);
+    console.log(array_answers);
+    for (let i = 0; i < array_number.length; i++) {
+        if (array_number.includes(array_answers[i])) {
+            array_result.push(array_answers[i]);
+        }
+    }
+    result.innerText = `Hai memorizzato ${array_result.length} numeri e sono ${array_result}`;
 }
--- js/script.js
+++ js/script.js
@@ -5,9 +5,9 @@ const numbers_list = document.querySelector('#numbers-list');
 const instructures = document.querySelector('#instructions');
 const countdown = document.querySelector('#countdown');
 const answers_form = document.querySelector('#answers-form');
-
-
-
+const list_answers = document.querySelectorAll('#input-group input');
+const result = document.querySelector('#message');
+const btn_confirm = document.querySelector('#btn-confirm');
 
 
 
@@ -18,3 +18,6 @@ for(let i = 0; i < random_number_array.length; i++){
 
 
 creaCountdown(5, countdown);
+btn_confirm.addEventListener('click',function(){
+    answers_check(random_number_array,list_answers);
+});
